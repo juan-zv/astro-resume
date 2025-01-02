309251fa8f9c7d23a9cca4ded78fec466879c597
juan-zv <juanzuritavasquez@gmail.com>
2025-01-01 19:54:50 -0800

    retry

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://juan-zv.github.io/',
-    base: 'astro-resume/',
+    site: 'https://juan-zv.github.io',
+    base: '/astro-resume/',
 });
